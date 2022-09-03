6ca9ad86e431e2e952d02f5dfa8ea51c5bcd2dc9
Johnny Howe <jonathon.o.howe@gmail.com>
2022-09-03 19:03:19 +1200

    initial build

---
+++ unlucky-duck/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="center">
+      <h1>AAAAAAAAAAA</h1>
+      <p>test build</p>
+      <p>joes moes toes foes crows shows hoes foes rows moes pose doze grows lows bows noes</p>
+    </div>
+  );
+}
+
+export default App;
